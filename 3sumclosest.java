c9f53fb37d676216adb5be6b43f39440a642b5e3
GambleGear <zhushiyao.zsy@gmail.com>
2013-11-12 17:03:15 -0500

    Update 3sumclosest.java

--- 3sumclosest.java
+++ 3sumclosest.java
@@ -29,3 +29,30 @@ public class Solution {
         
     }
 }
+
+// This is another version which is easer to understand
+public int threeSumClosest(int[] num, int target) {
+    if (num.length<3) { // if less than three items then return 0
+        return Integer.MAX_VALUE;
+    }
+    Arrays.sort(num);
+    int res = num[0]+num[1]+num[2];
+    for (int i=0; i<num.length-2; ++i) {
+        if (i>0 && num[i]==num[i-1])  continue;
+        int start = i+1, end = num.length-1;
+        while (start<end) {
+            int sum = num[i] + num[start] + num[end];
+            if (Math.abs(sum-target) < Math.abs(res-target)) {
+                res = sum;
+            }
+            if (sum == target) {
+                return res;
+            } else if (sum < target) {
+                start++;
+            } else {
+                end--;
+            }
+        }//end-while
+    }
+    return res;
+}
